6a7862debf5612b3e818e8d2f78811f05ce00745
Alexandre TO <alexandreto1996@gmail.com>
2024-10-21 12:44:32 +0200

    add user entity

---
+++ src/Entity/User.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity]
+#[ORM\Table(name: "Users")]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id;
+
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    #[Groups(["default", "create", "update"])]
+    #[Assert\NotBlank(groups: ["default", "create"])]
+    #[Assert\Length(min: 4, max: 180, groups: ["default", "create", "update"])]
+    private ?string $username = null;
+
+    #[ORM\Column(type: 'json')]
+    #[Groups(["default"])]
+    private array $roles = [];
+
+    #[ORM\Column(type: 'string')]
+    #[Groups(["create"])]
+    #[Assert\NotBlank(groups: ["default", "create"])]
+    private ?string $password = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // Guarantee every user at least has ROLE_USER
+        if (!in_array('ROLE_USER', $roles, true)) {
+            $roles[] = 'ROLE_USER';
+        }
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function eraseCredentials(): void
+    {
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+}
