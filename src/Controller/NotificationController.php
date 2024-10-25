fd9210002dc08b09ea55cc68e5c77ac755a203e6
Alexandre TO <alexandreto1996@gmail.com>
2024-10-25 13:31:33 +0200

    fix the notification controller issue

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Repository\NotificationRepository;
+use App\Entity\Notification;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -12,10 +13,10 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted('IS_AUTHENTICATED_FULLY')]
 class NotificationController extends AbstractController
 {
-    public function listUnreadNotifications(NotificationRepository $notificationRepository): Response
+    public function listUnreadNotifications(EntityManagerInterface $em): Response
     {
         $user = $this->getUser();
-        $notifications = $notificationRepository->findBy(['user' => $user, 'isRead' => false]);
+        $notifications = $em->getRepository(Notification::class)->findBy(['user' => $user, 'isRead' => false]);
 
         return $this->render('notifications/list.html.twig', [
             'notifications' => $notifications,
